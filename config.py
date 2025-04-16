b5880b292962e0b1f1977e4a5f4c13535f67aa67
Abdullah <143931418+abdullah001111@users.noreply.github.com>
2025-04-16 22:15:07 +0600

    Update config.py

--- config.py
+++ config.py
@@ -65,8 +65,8 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 #Short Url or Api
-SHORT_URL = os.environ.get("SHORTNER_URL", "kingurl.in")
-SHORT_API = os.environ.get("SHORTNER_API", "8e922946e7021fad80ff62a604a507a240f4531e")
+SHORT_URL = os.environ.get("SHORTNER_URL", "arolinks.com")
+SHORT_API = os.environ.get("SHORTNER_API", "97efe163e07453fe37fcd8a36adb284fb2adca2f")
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - @Adult_Mayhem"
